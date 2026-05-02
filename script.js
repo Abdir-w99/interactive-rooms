a0d6ea42b8f3e8898f257a605feda942b8d34b8a
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-05-02 19:25:25 +0200

    Add room images

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const rooms = [
   {
     name: "Hallway",
+    image: "./assets/images/hallway.jpg",
     description: "You are in the hallway.",
     options: ["Living room", "Bedroom"],
   },
   {
     name: "Kitchen",
+    image: "./assets/images/kitchen.jpg",
     description: "You are in the kitchen.",
     options: ["Living room"],
   },
   {
     name: "Bedroom",
+    image: "./assets/images/bedroom.jpg",
     description: "You are in the bedroom.",
     options: ["Hallway"],
   },
   {
     name: "Living room",
+    image: "./assets/images/living-room.jpg",
     description: "You are in the living room.",
     options: ["Kitchen", "Hallway"],
   },
@@ -25,9 +29,11 @@ let currentRoom = rooms[0];
 
 const description = document.querySelector("#description");
 const buttons = document.querySelector("#buttons");
+const roomImage = document.querySelector("#room-image");
 
 function showRoom() {
   description.textContent = currentRoom.description;
+  roomImage.src = currentRoom.image;
 
   buttons.innerHTML = "";
 
